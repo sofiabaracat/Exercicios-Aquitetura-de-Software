b6bcd6e0c4b2ab7dc5d238ffa0a931bfd70c737f
ggiselyy <gisely.macedo@gmail.com>
2018-08-21 00:31:39 -0300

    Atualizado Home e classe principal

--- Exercicio01/src/exercicio01/Exercicio01.java
+++ Exercicio01/src/exercicio01/Exercicio01.java
@@ -3,6 +3,7 @@ package exercicio01;
 import java.io.*;
 import java.util.*;
 import java.lang.*;
+import javax.swing.*;
 
 public class Exercicio01{
 
@@ -13,8 +14,10 @@ public class Exercicio01{
         Scanner scanner = new Scanner(System.in);
         while (true){
             System.out.println("\n( 1 ) - Cadastrar Nome");
-            System.out.println("\n( 2 ) - Consultar Nomes");
-        
+            System.out.println("\n( 2 ) - Cadastrar País");
+            System.out.println("\n( 3 ) - Consultar Nome");
+            System.out.println("\n( 4 ) - Consultar País");
+
             op = scanner.nextInt();
             
             switch (op){

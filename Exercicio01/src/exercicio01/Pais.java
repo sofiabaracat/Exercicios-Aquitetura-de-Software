6fc0b697f1e25ae021f1f812702507b6452e5c49
igorglousada <igorglousada@gmail.com>
2018-08-21 02:27:56 -0300

    Adicionado a classe Pais

---
+++ Exercicio01/src/exercicio01/Pais.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author igorl_000
+ */
+public class Pais {
+    
+    private String nome;
+    private String sigla;
+    private Integer codigo;
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome (String nome){
+        if(nome.equals("")){
+            JOptionPane.showMessageDialog(null, "Campo 'Nome' Obrigatório", "ERRO",JOptionPane.OK_OPTION);
+        }
+        else{
+            this.nome = nome;
+        }
+    }
+    
+    public String getSigla(){
+        return sigla;
+    }
+    
+    public void setSigla(String sigla) throws Exception{
+        if(sigla.equals("")){
+            JOptionPane.showMessageDialog(null, "Campo 'Sigla' Obrigatório", "ERRO",JOptionPane.OK_OPTION);
+        }
+        else{
+            this.sigla = sigla;
+        }
+    }
+    
+    public Integer getCodigo(){
+        return codigo;
+    }
+    
+    public void setCodigo(Integer codigo) throws Exception{
+        try{
+                codigo = Integer.parseInt("");
+            }
+        catch(NumberFormatException a){
+            JOptionPane.showMessageDialog(null, "Somente números", "ERRO",JOptionPane.OK_OPTION);
+        }
+        
+        if(codigo.equals(null)){
+            JOptionPane.showMessageDialog(null, "Campo 'Código' Obrigatório", "ERRO",JOptionPane.OK_OPTION);
+        }
+        else{
+            this.codigo = codigo;
+        }
+    }
+}
